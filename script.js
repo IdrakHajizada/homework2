51bb70de257994c48c597e6442158863f0520fcd
IdrakHajizada <94737790+IdrakHajizada@users.noreply.github.com>
2022-08-15 00:04:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Task No 2
+ function Start(){
+    let a = 4;
+    let b = 3;
+    let answer = 0
+
+    if(a > b){
+        let answer = true
+    }
+    else{
+        let answer = false
+    }
+
+    answer
+        ? alert('false')
+        : alert('true')    
+ }
+
+
+
+
